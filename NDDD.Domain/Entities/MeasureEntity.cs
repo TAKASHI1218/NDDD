98360cc94cb34cc2bd62310560f873c40a3eca7a
橋谷　崇 <t61342006@yahoo.co.jp>
2025-10-26 15:14:21 +0900

    テストコード作成

---
+++ NDDD.Domain/Entities/MeasureEntity.cs
@@ -0,0 +1,36 @@
+﻿using NDDD.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Entities
+{
+    public sealed class MeasureEntity : IMeasureRepository
+    {
+        public MeasureEntity(int areaId, DateTime measureDate, float measureValue) 
+        {
+            AreaId = areaId;
+            MeasureDate = measureDate;
+            MeasureValue = measureValue;
+        }
+
+
+        public MeasureEntity GetLatest()
+        {
+            return new MeasureEntity(AreaId, MeasureDate, MeasureValue);
+        }
+
+
+        public int AreaId { get; }
+
+        public DateTime MeasureDate { get; }
+
+        public float MeasureValue { get; }
+
+
+
+
+    }
+}
---
+++ NDDD.Domain/Repositories/IMeasureRepository.cs
@@ -0,0 +1,15 @@
+﻿using NDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Repositories
+{
+    public interface IMeasureRepository
+    {
+        MeasureEntity GetLatest();
+
+    }
+}
