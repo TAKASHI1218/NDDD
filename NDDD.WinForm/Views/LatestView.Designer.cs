bd903490eecb04a7da27e929502dcd7819ecc5dc
橋谷　崇 <t61342006@yahoo.co.jp>
2025-10-26 22:58:43 +0900

    WindowsFormのLatestViewとMeasureFakeの作成

---
+++ NDDD.WinForm/Views/LatestView.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace NDDD.WinForm.Views
+{
+    partial class LatestView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.AreaIdTextBox = new System.Windows.Forms.TextBox();
+            this.MeasureValueTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MeasureDateTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "エリアId";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // AreaIdTextBox
+            // 
+            this.AreaIdTextBox.Location = new System.Drawing.Point(132, 12);
+            this.AreaIdTextBox.Name = "AreaIdTextBox";
+            this.AreaIdTextBox.ReadOnly = true;
+            this.AreaIdTextBox.Size = new System.Drawing.Size(186, 25);
+            this.AreaIdTextBox.TabIndex = 2;
+            // 
+            // MeasureValueTextBox
+            // 
+            this.MeasureValueTextBox.Location = new System.Drawing.Point(132, 85);
+            this.MeasureValueTextBox.Name = "MeasureValueTextBox";
+            this.MeasureValueTextBox.ReadOnly = true;
+            this.MeasureValueTextBox.Size = new System.Drawing.Size(186, 25);
+            this.MeasureValueTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "計測値";
+            // 
+            // MeasureDateTextBox
+            // 
+            this.MeasureDateTextBox.Location = new System.Drawing.Point(132, 48);
+            this.MeasureDateTextBox.Name = "MeasureDateTextBox";
+            this.MeasureDateTextBox.ReadOnly = true;
+            this.MeasureDateTextBox.Size = new System.Drawing.Size(186, 25);
+            this.MeasureDateTextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "計測日時";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(26, 137);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(298, 39);
+            this.SearchButton.TabIndex = 7;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // LatestView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 230);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.MeasureDateTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MeasureValueTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AreaIdTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "LatestView";
+            this.Text = "LatestView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox AreaIdTextBox;
+        private System.Windows.Forms.TextBox MeasureValueTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MeasureDateTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SearchButton;
+    }
+}
---
+++ NDDD.WinForm/Views/LatestView.cs
@@ -0,0 +1,53 @@
+﻿using NDDD.Domain.Repositories;
+using NDDD.Infrastructure.Fake;
+using NDDD.WinForm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NDDD.WinForm.Views
+{
+    public partial class LatestView : Form
+    {
+
+        private LatestViewModel _viewModel = new LatestViewModel(new MeasureFake());
+        public LatestView()
+        {
+            InitializeComponent();
+
+            AreaIdTextBox.DataBindings.Add(
+                "Text", 
+                _viewModel, 
+                nameof(_viewModel.AreaIdText)
+                );
+
+            MeasureDateTextBox.DataBindings.Add(
+                "Text",
+                _viewModel,
+                 nameof(_viewModel.MeasureDateText)
+                );
+
+            MeasureValueTextBox.DataBindings.Add(
+           "Text",
+           _viewModel,
+            nameof(_viewModel.MeasureValueText)
+           );
+
+
+
+
+
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            _viewModel.Search();
+        }
+    }
+}
