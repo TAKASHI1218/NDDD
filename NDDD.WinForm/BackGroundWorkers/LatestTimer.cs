73678bc0285905a3fa8c087e10335a56d445755c
橋谷　崇 <t61342006@yahoo.co.jp>
2025-11-03 00:46:30 +0900

    タイマー処理追加

---
+++ NDDD.WinForm/BackGroundWorkers/LatestTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace NDDD.WinForm.BackGroundWorkers
+{
+    internal static class LatestTimer
+    {
+        private static System.Threading.Timer _timer;
+        private static bool _isWork = false;
+
+        static LatestTimer()
+        {
+            _timer = new Timer(Callback);
+        }
+
+        internal static void Start()
+        {
+            _timer.Change(10000, 10000);
+        }
+
+        internal static void Stop()
+        {
+            _timer.Change(Timeout.Infinite,Timeout.Infinite);
+        }
+
+        private static void Callback(object o)
+        {
+            if (_isWork)
+            {
+                return;
+            }
+
+            try
+            {
+                _isWork = true; ;
+
+            }
+            finally
+            {
+                _isWork = false;
+            }
+        }
+    }
+}
